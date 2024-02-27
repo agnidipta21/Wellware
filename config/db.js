880a0a20cf1c74f77b37b4122a2d9a20b8fe27d7
agnidipta21 <agnidipta21@gmal.com>
2024-02-27 22:12:48 +0530

    Initial Commit

--- config/db.js
+++ config/db.js
@@ -1,21 +1,24 @@
-const { MongoClient } = require('mongodb');
-​
-// MongoDB connection URL. Make sure to replace <connection_string> with your MongoDB connection string.
-const uri = 'mongodb://localhost:27017/admin';
-​
-// Connect to MongoDB
-async function connectToMongoDB() {
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017"; // Replace with your MongoDB URI
+const dbName = "admin"; // Replace with your database name
+
+async function connectDB() {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   try {
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
-​
-    // Access the admin database
-    console.log('connected to mongodb')
-    client.close();
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.log("Connected to the MongoDB server");
+
+    // Use the connected client for further operations if needed
+    // e.g., client.db(dbName).collection('your_collection_name').find({}).toArray();
+  } finally {
+    await client.close();
+    console.log("Connection to MongoDB closed");
   }
 }
-​
-// Call the connectToMongoDB function to start the connection
-connectToMongoDB();
+
+module.exports = connectDB;
